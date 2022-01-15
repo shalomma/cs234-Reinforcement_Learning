fd18670a1781a3419769ed988176adb3d52cebd5
Maayan Shalom <sha.maayan@gmail.com>
2022-01-15 10:43:57 +0200

    q6

--- assignment2/q6_train_atari_nature.py
+++ assignment2/q6_train_atari_nature.py
@@ -29,16 +29,16 @@ if __name__ == '__main__':
     # make env
     env = gym.make(config.env_name)
     env = MaxAndSkipEnv(env, skip=config.skip_frame)
-    env = PreproWrapper(env, prepro=greyscale, shape=(80, 80, 1), 
+    env = PreproWrapper(env, prepro=greyscale, shape=(80, 80, 1),
                         overwrite_render=config.overwrite_render)
 
     # exploration strategy
-    exp_schedule = LinearExploration(env, config.eps_begin, 
-            config.eps_end, config.eps_nsteps)
+    exp_schedule = LinearExploration(env, config.eps_begin,
+                                     config.eps_end, config.eps_nsteps)
 
     # learning rate schedule
-    lr_schedule  = LinearSchedule(config.lr_begin, config.lr_end,
-            config.lr_nsteps)
+    lr_schedule = LinearSchedule(config.lr_begin, config.lr_end,
+                                 config.lr_nsteps)
 
     # Check weights
     load_path = Path(config.load_path)
